79c544357da235e8fd57978734aec9811aa333da
Andrea Pedemonte <andrea.pedemonte@hometogo.com>
2023-02-20 15:53:04 +0100

    Test commit

--- R/OnLoad.R
+++ R/OnLoad.R
@@ -3,6 +3,7 @@
   with(conf, {
     home_folder = '/Users/andrea.pedemonte'
     pyenv_folder = file.path(home_folder, '.pyenv/versions')
+    a_var = 1
   })
   lockEnvironment(conf)
   assign('conf', conf, envir = globalenv())
