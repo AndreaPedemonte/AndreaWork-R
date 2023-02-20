2104f44f0279e2963c31189751528ba1efc7866e
Andrea Pedemonte <andrea.pedemonte@hometogo.com>
2023-02-20 16:34:34 +0100

    Test

--- R/OnLoad.R
+++ R/OnLoad.R
@@ -4,6 +4,7 @@
     home_folder = '/Users/andrea.pedemonte'
     pyenv_folder = file.path(home_folder, '.pyenv/versions')
     downloads = file.path(home_folder, 'Downloads')
+    test = 1
   })
   lockEnvironment(conf)
   assign('conf', conf, envir = globalenv())
