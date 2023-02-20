7deb2c68f2cbf1f0854d05afba96bd58d55f5016
Andrea Pedemonte <andrea.pedemonte@hometogo.com>
2023-02-20 16:35:27 +0100

    Revert test - git on Rstudio works OK

--- R/OnLoad.R
+++ R/OnLoad.R
@@ -4,7 +4,6 @@
     home_folder = '/Users/andrea.pedemonte'
     pyenv_folder = file.path(home_folder, '.pyenv/versions')
     downloads = file.path(home_folder, 'Downloads')
-    test = 1
   })
   lockEnvironment(conf)
   assign('conf', conf, envir = globalenv())
